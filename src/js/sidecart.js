2883271172e11301ef0d78d4acea9b334c9bf3ee
Tyler <71272314+https-ty@users.noreply.github.com>
2024-07-26 13:51:13 +0800

    fix color

--- src/js/sidecart.js
+++ src/js/sidecart.js
@@ -1,30 +1,64 @@
-const openBtn = document.getElementById('open-cart-btn')
-const cart = document.getElementById('sidecart')
-const closeBtn = document.getElementById('close-button')
-const backdrop = document.querySelector('.backdrop')
+// const openBtn = document.getElementById('open-cart-btn')
+// const cart = document.getElementById('sidecart')
+// const closeBtn = document.getElementById('close-button')
+// const backdrop = document.querySelector('.backdrop')
 
-openBtn.addEventListener('click', openCart)
-closeBtn.addEventListener('click', closeCart)
-backdrop.addEventListener('click', closeCart)
+// openBtn.addEventListener('click', openCart)
+// closeBtn.addEventListener('click', closeCart)
+// backdrop.addEventListener('click', closeCart)
 
-//Open Cart
-function openCart(){
-    cart.classList.add('open')
-    backdrop.style.display = 'block'
+// //Open Cart
+// function openCart(){
+//     cart.classList.add('open')
+//     backdrop.style.display = 'block'
     
-    setTimeout(() =>{
-        backdrop.classList.add('show')
-    }, 0)
+//     setTimeout(() =>{
+//         backdrop.classList.add('show')
+//     }, 0)
     
+// }
+
+// //Close Cart
+// function closeCart(){
+//     cart.classList.remove('open')
+//     backdrop.classList.remove('show')
+
+//     setTimeout(() =>{
+//         backdrop.style.display = 'none'
+//     }, 500)
+// }
+
+const openBtn = document.getElementById('open-cart-btn');
+const cart = document.getElementById('sidecart');
+const closeBtn = document.getElementById('close-button');
+const backdrop = document.querySelector('.backdrop');
+
+openBtn.addEventListener('click', openCart);
+closeBtn.addEventListener('click', closeCart);
+backdrop.addEventListener('click', closeCart);
+
+// Open Cart
+function openCart() {
+    cart.classList.add('open');
+    backdrop.style.display = 'block';
+    
+    setTimeout(() => {
+        backdrop.classList.add('show');
+    }, 0);
+
+    // Hide scrollbars
+    document.body.classList.add('no-scroll');
 }
 
-//Close Cart
-function closeCart(){
-    cart.classList.remove('open')
-    backdrop.classList.remove('show')
+// Close Cart
+function closeCart() {
+    cart.classList.remove('open');
+    backdrop.classList.remove('show');
 
-    setTimeout(() =>{
-        backdrop.style.display = 'none'
-    }, 500)
+    setTimeout(() => {
+        backdrop.style.display = 'none';
+    }, 500);
+
+    // Show scrollbars
+    document.body.classList.remove('no-scroll');
 }
-
