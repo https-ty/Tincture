5899e3b5adca8f7946cc1e007a1311aeab39cbd8
Tyler <71272314+https-ty@users.noreply.github.com>
2024-07-26 16:06:40 +0800

    idk

--- src/js/sweet-alerts.js
+++ src/js/sweet-alerts.js
@@ -1,26 +1,51 @@
-document.querySelectorAll('.cart-button').forEach(button => {
-    button.addEventListener('click', function(event) {
-      event.preventDefault(); // Prevent the default anchor behavior
+document.querySelectorAll(".cart-button").forEach((button) => {
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
 
-      // SweetAlert
-      swal("Added to Cart!", "You have successfully added the item to your cart.", "success");
-    });
+    // SweetAlert
+    swal(
+      "Added to Cart!",
+      "You have successfully added the item to your cart.",
+      "success"
+    );
   });
+});
 
-  document.querySelectorAll('.submit-button').forEach(button => {
-    button.addEventListener('click', function(event) {
-      event.preventDefault(); // Prevent the default anchor behavior
+document.querySelectorAll(".submit-button").forEach((button) => {
+  button.addEventListener("click", function (event) {
+    event.preventDefault(); 
 
-      // SweetAlert
-      swal("Email Sent!", "You have successfully sent an email to Tincture.", "success");
-    });
+    // SweetAlert
+    swal(
+      "Email Sent!",
+      "You have successfully sent an email to Tincture.",
+      "success"
+    );
   });
+});
 
-  document.querySelectorAll('.checkout-button').forEach(button => {
-    button.addEventListener('click', function(event) {
-      event.preventDefault(); // Prevent the default anchor behavior
+document.querySelectorAll(".checkout-button").forEach((button) => {
+  button.addEventListener("click", function (event) {
+    event.preventDefault(); 
 
-      // SweetAlert
-      swal("Checkout Complete!", "You have successfully completed your purchase.", "success");
-    });
+    // SweetAlert
+    swal(
+      "Checkout Complete!",
+      "You have successfully completed your purchase.",
+      "success"
+    );
   });
+});
+
+document.querySelectorAll(".add-to-cart").forEach((button) => {
+  button.addEventListener("click", function (event) {
+    event.preventDefault(); 
+
+    // SweetAlert
+    swal(
+      "Checkout Complete!",
+      "You have successfully completed your purchase.",
+      "success"
+    );
+  });
+});
